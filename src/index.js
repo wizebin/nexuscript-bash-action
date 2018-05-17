9757f2dc17de7e62a7860418ccd29b26d1d71935
Stephen <wizebin@gmail.com>
2018-05-16 22:58:34 -0400

    Changed to new callWrap function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-import nexusdk from 'nexusdk';
+import { wrapAction } from 'nexusdk';
 import { exec } from 'child_process';
 
-function callWrap(messageType) {
-  return function (message) {
-    nexusdk.sendMessage({ type: messageType, data: message });
-  }
-}
-
-nexusdk.on('configuration', () => {
-  return {
-    command: { required: true, _type: 'string' },
-    timeout: { required: false, _type: 'number' },
-    env: { required: false, _type: 'object', description: 'key value environment variables to send to process' },
-  };
-});
-
-nexusdk.on('execute', (properties) => {
-  const { command, timeout, env } = properties;
-  const proc = exec(command, { windowsHide: true, timeout, env, stdio: 'pipe' });
-  proc.stdout.on('data', callWrap('log'));
-  proc.stderr.on('data', callWrap('err'));
-  proc.stdout.on('close', callWrap('close'));
-});
+wrapAction((properties, sendMessage) => {
+  return new Promise(resolve => {
+    const { command, timeout, env } = properties;
+    const proc = exec(command, { windowsHide: true, timeout, env, stdio: 'pipe' });
+    proc.stdout.on('data', (data) => sendMessage('log', data));
+    proc.stderr.on('data', (data) => sendMessage('err', data));
+    proc.on('exit', (code) => {
+      resolve(code);
+    });
+  });
+})
