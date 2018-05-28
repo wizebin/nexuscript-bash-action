5c0f00f0f3dac3ee2197660ab8f365b81c736f59
Stephen <wizebin@gmail.com>
2018-05-28 16:50:16 -0400

    Using wrapAction correctly

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { wrapAction } from 'nexusdk';
 import { exec } from 'child_process';
 
-wrapAction((properties, sendMessage) => {
+wrapAction((properties, event) => {
   return new Promise(resolve => {
     const { command, timeout, env } = properties;
     const proc = exec(command, { windowsHide: true, timeout, env, stdio: 'pipe' });
-    proc.stdout.on('data', (data) => sendMessage('log', data));
-    proc.stderr.on('data', (data) => sendMessage('err', data));
+    const results = { stdout: [], stderr: [], code: -1 };
+    proc.stdout.on('data', (data) => results.stdout.push(data));
+    proc.stderr.on('data', (data) => results.stderr.push(data));
     proc.on('exit', (code) => {
-      resolve(code);
+      results.code = code;
+      resolve(results);
     });
   });
 })
